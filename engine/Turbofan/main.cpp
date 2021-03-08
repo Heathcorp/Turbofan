4552095175d6d4dfe9242eb1a57937a1783b5fb3
HeathManning <heath@manningf.com>
2021-03-08 22:35:38 +1100

    minor change to test git remote pushing

--- engine/Turbofan/main.cpp
+++ engine/Turbofan/main.cpp
@@ -6,7 +6,15 @@ int main() {
 }
 
 struct BoardState {
-    unsigned int 
+    unsigned int rank8 : 32;
+    unsigned int rank7 : 32;
+    unsigned int rank6 : 32;
+    unsigned int rank5 : 32;
+    unsigned int rank4 : 32;
+    unsigned int rank3 : 32;
+    unsigned int rank2 : 32;
+    unsigned int rank1 : 32;
+
 }
 
 int FENToBit
